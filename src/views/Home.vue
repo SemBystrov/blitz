84ae0179270a8862cbe3e568c17bd19dd8efa063
SemBystrov <sembystrov14@gmail.com>
2020-05-03 22:05:38 +0300

    blitz v.1.3, fix, add progress and done_task_list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,24 +35,37 @@
               {{ get_actual_task.text }}
             </v-card-text>
 
+            <template v-if="activators.end_time">
+              <v-bottom-navigation class="elevation-0 mt-8 mb-8">
+                <v-btn @click="to_end_task" text v-if="this.task_list.length > 1">
+                  <span>В очередь</span>
+                  <v-icon>mdi-alarm-snooze</v-icon>
+                </v-btn>
+                <v-btn @click="refresh_countdown">
+                  <span>Продолжить</span>
+                  <v-icon>mdi-alarm-plus</v-icon>
+                </v-btn>
+                <v-btn icon @click="task_done">
+                  <span class="green--text text--darken-2">Готово</span>
+                  <v-icon class="green--text text--darken-2">mdi-alarm-check</v-icon>
+                </v-btn>
+              </v-bottom-navigation>
 
-            <v-bottom-navigation v-if="activators.end_time" class="elevation-0 mt-8 mb-8">
-              <v-btn @click="to_end_task">
-                <span>В очередь</span>
-                <v-icon>mdi-alarm-snooze</v-icon>
-              </v-btn>
-              <v-btn @click="refresh_countdown">
-                <span>Продолжить</span>
-                <v-icon>mdi-alarm-plus</v-icon>
-              </v-btn>
-              <v-btn icon @click="delete_task(0)">
-                <span class="green--text text--darken-2">Готово</span>
-                <v-icon class="green--text text--darken-2">mdi-alarm-check</v-icon>
-              </v-btn>
-            </v-bottom-navigation>
+              <v-card flat class="pa-3 pa-sm-12 pt-4">
+                <v-card-title>
+                  Сделано за сеанс:
+                </v-card-title>
+                <v-list>
+                  <v-list-item-group>
+                    <v-list-item v-for="t in done_task_list" :key="'done-' + t.title">
+                      {{ t.title }}
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </v-card>
 
-
-            <v-slider v-else v-model="actual_task.passed" min="0" max="10" append-icon="mdi-alarm" class="ml-3 mr-3" color="black" track-color="grey">
+            </template>
+            <v-slider v-else v-model="actual_task.passed" min="0" max="2400" append-icon="mdi-alarm" class="ml-3 mr-3" color="black" track-color="grey" @change="change_progress">
 
             </v-slider>
 
@@ -102,8 +115,8 @@
     </v-fab-transition>
 
 
-    <v-bottom-sheet v-model="activators.setting" fullscreen class="bg">
-      <v-sheet class="bg">
+    <v-bottom-sheet v-model="activators.setting" fullscreen class="bg" scrollable>
+      <v-card white class="bg">
         <v-btn
           color="black"
           class="ma-1 ma-sm-8 ma-md-12"
@@ -118,26 +131,36 @@
         >
           <v-icon>mdi-close</v-icon>
         </v-btn>
-        <v-row justify="center">
-          <v-col cols="8">
-            <v-list v-if="task_list.length > 0">
-
-              <draggable v-model="task_list" @change="update_task">
-                  <v-list-item v-for="(task, index) in task_list" :key="task.title" class="elevation-1 mt-2">
-                    {{ task.title }}
-                    <v-spacer></v-spacer>
-                    <v-btn :key="'task-item-btn-' + task.title" icon @click="delete_task(index)"><v-icon>mdi-close</v-icon></v-btn>
-                  </v-list-item>
-              </draggable>
+        <v-card-text>
+          <v-container>
+            <v-row justify="center">
+              <v-col cols="8">
 
 
-            </v-list>
-            <v-alert v-else>Задач нет</v-alert>
-            <v-btn class="mt-4" @click.stop="activators.add_task = true"><v-icon>mdi-plus</v-icon>Добавить</v-btn>
-          </v-col>
-        </v-row>
 
-      </v-sheet>
+                <v-list v-if="task_list.length > 0">
+
+                  <draggable v-model="task_list" @change="update_task">
+                      <v-list-item v-for="(task, index) in task_list" :key="task.title" class="elevation-1 mt-2">
+                        {{ task.title }}
+                        <v-spacer></v-spacer>
+                        <v-btn :key="'task-item-btn-' + task.title" icon @click="delete_task(index)"><v-icon>mdi-close</v-icon></v-btn>
+                      </v-list-item>
+                  </draggable>
+
+
+                </v-list>
+                <v-alert v-else>Задач нет</v-alert>
+                </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn x-large class="ma-12" @click.stop="activators.add_task = true"><v-icon>mdi-plus</v-icon>Добавить</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
     </v-bottom-sheet>
 
 
@@ -173,7 +196,7 @@
 import draggable from 'vuedraggable'
 import {get, set} from 'idb-keyval'
 
-const min40 = 10002; //2400002
+const min40 = 2400002;
 
 export default {
   name: 'Home',
@@ -191,22 +214,23 @@ export default {
 
       new_task: {
         title: '',
-        text: '',
-        time: min40
+        text: ''
       },
       actual_task: {
         title: '',
         text: '',
+        passed: 0,
         time: min40,
-        passed: 0
       },
       task_list: [],
+      done_task_list: [],
       refresh: 1,
+      refresh_passed: 0,
     }
   },
   async created () {
     let val = await get('task_list');
-    if (typeof val !== 'undefined' && val.length === 0){
+    if (typeof val !== 'undefined' && val.length !== 0){
       this.task_list = val;
       this.update_task();
     }
@@ -218,14 +242,20 @@ export default {
     has_actual_task: ({task_list}) => {
       return task_list.length > 0
     },
-    get_left_time ({actual_task, refresh}) {
-      return actual_task.time + refresh;
+    get_left_time ({actual_task, refresh, refresh_passed}) {
+      return actual_task.time + refresh - refresh_passed;
     }
   },
   methods: {
 
     refresh_countdown (){
       this.actual_task.passed = 0;
+      this.refresh_passed = 0;
+      this.refresh = (-1) * this.refresh;
+    },
+
+    change_progress () {
+      this.refresh_passed = this.actual_task.passed * 1000
       this.refresh = (-1) * this.refresh;
     },
 
@@ -246,6 +276,15 @@ export default {
       this.update_task();
     },
 
+    task_done () {
+      let done_task = {
+        ...this.task_list[0]
+      }
+      this.done_task_list.unshift(done_task);
+      this.delete_task(0);
+      console.log(this.done_task_list);
+    },
+
     to_end_task () {
       let task = {
         ...this.task_list[0]
@@ -256,9 +295,10 @@ export default {
     },
 
     update_task () {
-      if (!(this.actual_task.title === this.task_list[0].title && this.actual_task.text === this.task_list[0].text)) {
+      if ((this.has_actual_task) && !(this.actual_task.title === this.task_list[0].title && this.actual_task.text === this.task_list[0].text)) {
         let task = {
           passed: 0,
+          time: min40,
           ...this.task_list[0]
         };
         this.actual_task = task;
